fd374d3aefce2493b0668719bb04be780f4774b6
nyashapasipamire <149308926+nyashapasipamire@users.noreply.github.com>
2023-11-09 09:59:38 +0100

    Add files via upload

--- array.java
+++ array.java
@@ -16,7 +16,25 @@ public class array {
                 int greater = array[j + 1];
                 array[j] = array[j + 1];
                 array[j + 1] = greater;
+
+
             }
+            boolean toSort = true;
+            for (int i = 0; i < array.length - 1; i++) {
+                toSort = false;
+                counter++;
+                for (j = 0; j < array.length - 1; j++) {
+                    if (array[j] > array[j + 1]) {
+                        counter++;
+                        int greater = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = greater;
+                        toSort = true;
+                    }
+                }
+                System.out.println(Arrays.toString(array));
+            }
+            System.out.println(counter +"operation");
+
         }
-    }
-}
+    }}
