2c01ee36383e39d0a04e3f751619773049d1aae3
nyashapasipamire <149308926+nyashapasipamire@users.noreply.github.com>
2023-11-16 09:32:51 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println();
+        Staff staff = new Staff();
+        staff.initialize();
+        System.out.println("Staff information");
+        staff.print();
+}}
